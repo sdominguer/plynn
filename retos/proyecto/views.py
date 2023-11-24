303106ebcece3bb4bc407404cb90c20c8198a0fa
sdominguer <sdominguer@eafit.edu.co>
2023-11-24 13:18:48 -0500

    update

--- retos/proyecto/views.py
+++ retos/proyecto/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    grupo = request.GET.get('grupo', '')
+    return render(request, 'home.html', {'grupo': grupo})
+
+def login(request):
+    grupos = ['B&A Beauty', 'La Molienda Panelera', 'Hotel MOA', 'Superverfru', 'INPRO', 'PETIT', 'ALNE', 'Alcaldía de Ronda', 'Mundopiel', 'TECH']
+
+    if request.method == 'GET':
+        return render(request, 'login.html', {'grupos': grupos})
--- retos/retos/urls.py
+++ retos/retos/urls.py
@@ -20,4 +20,5 @@ from proyecto import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
 ]
